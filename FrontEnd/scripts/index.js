a1a6b754f6e56ccfad8c51b6f6e171ce2ec8dfa6
GripFast-III <jeandbdp@gmail.com>
2024-02-19 03:32:09 +0100

    Tri des projets; création de la page log in + CSS

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,16 +1,24 @@
+let allWorks = [];
+
 const init = async () => {
-  let finalResponse = await (
-    await fetch("http://localhost:5678/api/categories")
-  ).json();
+  let finalResponseCat = await fetchCategories();
+  let finalResponseWorks = await fetchWorks();
+  //await (
+  //await fetch("http://localhost:5678/api/categories")
+  //).json();
   //let response = await fetch("http://localhost:5678/api/categories");
   //let finalResponse = await response.json();
   //Ces deux lignes sont représentées par la ligne 2
 
-  finalResponse.unshift({
+  finalResponseCat.unshift({
     id: 0,
     name: "Tous",
   }); // Ajoute un nouvel item dans le array
-  console.log("🚀 ~ init ~ finalResponse:", finalResponse);
+  console.log("🚀 ~ init ~ finalResponseCat:", finalResponseCat);
+  console.log("🚀 ~ init ~ finalResponseWorks:", finalResponseWorks);
+  displayWorks(finalResponseWorks);
+  allWorks = finalResponseWorks;
+  displayButtons(finalResponseCat);
 };
 
 init();
@@ -19,11 +27,13 @@ init();
 async function fetchWorks() {
   try {
     const response = await fetch("http://localhost:5678/api/works");
-    const worksData = await response.json();
+    console.log("🚀 ~ fetchWorks ~ response:", response);
+    let worksData = null;
+    if (response.ok) worksData = await response.json();
+    else throw new Error("Pas de réponse de notre serveur");
     return worksData;
   } catch (error) {
     console.log("🚀 ~ fetchWorks ~ error:", error);
-    console.error("Erreur lors de la récupération des projets :", error);
   }
 }
 
@@ -38,34 +48,45 @@ async function fetchCategories() {
   }
 }
 
-// Etape 2 : Ajouter les méthodes display pour afficher les travaux
+// Étape 2 : Ajouter les méthodes display pour afficher les travaux
 function displayWorks(works) {
   const gallery = document.querySelector(".gallery");
   gallery.innerHTML = ""; // Efface le contenu précédent de la galerie
   works.forEach((work) => {
     const figure = document.createElement("figure");
     figure.setAttribute("data-categories", work.categoryId);
-    const img = document.createElement("img");
-    img.src = work.imageUrl;
-    img.alt = work.title;
-    const figcaption = document.createElement("figcaption");
-    figcaption.textContent = work.title;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
+    figure.innerHTML = `<img src="${work.imageUrl}" alt="${work.title}"/ ><figcaption>${work.title}</figcaption>`;
     gallery.appendChild(figure);
   });
 }
 
-// Etape 3 : Ajouter les méthodes display pour afficher les boutons
+// Étape 3 : Ajouter les méthodes display pour afficher les boutons
 function displayButtons(categories) {
-  const nav = document.querySelector("nav ul");
-  nav.innerHTML = ""; // Efface le contenu précédent de la navigation
+  const filters = document.querySelector(".buttons");
+  filters.innerHTML = ""; // Efface le contenu précédent de la filtersigation
   categories.forEach((category) => {
-    const li = document.createElement("li");
+    const li = document.createElement("li"); // À changer par une creation d'élément avec createElement
     const button = document.createElement("button");
-    button.textContent = category.name;
+    const paragraph = document.createElement("p"); // Crée un élément <p>
+    const buttonText = document.createTextNode(category.name); // Crée un nœud texte avec le nom de la catégorie
+
     button.addEventListener("click", () => filterWorksByCategory(category.id));
+    //button.textContent = category.name;
+    paragraph.appendChild(buttonText); // Ajoute le texte dans l'élément <p>
+    button.appendChild(paragraph); // Ajoute l'élément <p> à l'intérieur du "button"
     li.appendChild(button);
-    nav.appendChild(li);
+    filters.appendChild(li);
   });
 }
+
+const filterWorksByCategory = (id) => {
+  console.log("id", id);
+  console.log("allWorks", allWorks);
+  if (id !== 0) {
+    let filteredWorks = allWorks.filter((work) => work.categoryId === id);
+    console.log("🚀 ~ filterWorksByCategory ~ filteredWorks:", filteredWorks);
+    displayWorks(filteredWorks);
+  } else {
+    displayWorks(allWorks);
+  }
+};
